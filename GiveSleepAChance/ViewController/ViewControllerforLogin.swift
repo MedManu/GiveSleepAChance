cbba6d85b0c336cf02daa0ff0db5b565d735a3e2
Manuel Medwed <manuel.medwed@steadysense.at>
2019-02-14 11:55:58 +0100

    add some images an a new view for the login

---
+++ GiveSleepAChance/ViewController/ViewControllerforLogin.swift
@@ -0,0 +1,30 @@
+//
+//  ViewControllerforLogin.swift
+//  GiveSleepAChance
+//
+//  Created by Manuel Medwed on 14.02.19.
+//  Copyright © 2019 Manuel Medwed. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerforLogin: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
