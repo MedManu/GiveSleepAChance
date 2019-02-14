69137f6eb77722c2b161e8e482f6dc69ac7dfbc5
Manuel Medwed <manuel.medwed@steadysense.at>
2019-02-14 08:51:40 +0100

    Initial Commit

---
+++ GiveSleepAChance/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GiveSleepAChance
+//
+//  Created by Manuel Medwed on 14.02.19.
+//  Copyright © 2019 Manuel Medwed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
