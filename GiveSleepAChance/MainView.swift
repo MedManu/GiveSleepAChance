ed44e47033fd6172a06c2c272da6d2b7b7a416f9
Manuel Medwed <manuel.medwed@steadysense.at>
2019-04-23 20:13:49 +0200

    added Main View

---
+++ GiveSleepAChance/MainView.swift
@@ -0,0 +1,13 @@
+//
+//  MainView.swift
+//  GiveSleepAChance
+//
+//  Created by Manuel Medwed on 23.04.19.
+//  Copyright © 2019 Manuel Medwed. All rights reserved.
+//
+
+import UIKit
+
+class MainView: NSObject {
+
+}
